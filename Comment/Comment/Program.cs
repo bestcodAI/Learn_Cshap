89bd0c5acf16574708c9edd78d2fe0d78ec98e30
Chou Chamnan <109691277+bestcodAI@users.noreply.github.com>
2023-06-04 12:11:58 +0700

    first commit

---
+++ Comment/Comment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This comment line using single-line
+            Console.WriteLine("Hello Cambodia!!💕💕");
+            /* This is comments using multi-comment */
+            Console.WriteLine("My name is chamnna . I come from svayrieng province!❤✌👌");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Output/Output/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? "); // You can add as many WriteLine() methods as you want. Note that it will add a new line for each method
+            Console.WriteLine("How are you today? ");
+            Console.Write("My name is chamnan "); //The only difference is that it does not insert a new line at the end of the output:
+            Console.Write("and You");
+            Console.ReadKey(); // ReadKey() Obtains the next character or function key pressed by the user
+        }
+    }
+}
